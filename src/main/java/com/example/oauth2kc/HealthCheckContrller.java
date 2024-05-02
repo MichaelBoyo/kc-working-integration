83b6ac0ba97cfed5f976068864e34265f382ed52
michaelboyo <103725264+MichaelBoyo@users.noreply.github.com>
2024-05-02 08:22:08 +0100

    Update HealthCheckContrller.java

--- src/main/java/com/example/oauth2kc/HealthCheckContrller.java
+++ src/main/java/com/example/oauth2kc/HealthCheckContrller.java
@@ -1,1 +1,20 @@
 
+package com.example.oauth2kc;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+public class HealthCheckContrller {
+
+    List<String> todos = List.of("Buy Groceries", "Floating Berries");
+
+    @GetMapping
+    public List<String> geTodos() {
+        return todos;
+    }
+}
